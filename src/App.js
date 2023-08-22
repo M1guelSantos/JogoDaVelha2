2e58f52cecdedd3b107e3176d288ef832283883d
Miguel dos Santos <migueldossantos0096@gmail.com>
2023-08-22 00:37:25 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          EU SO MALUCO
         </p>
         <a
           className="App-link"
