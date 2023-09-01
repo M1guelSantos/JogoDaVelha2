6da126a1f4a50da93b4a695a5d71f3ed39a06157
Miguel dos Santos <migueldossantos0096@gmail.com>
2023-09-01 00:34:55 -0300

    menu object adicionado

---
+++ src/objects/Menu/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css'
+
+const index = () => {
+  return (
+    <>
+    <a href='#menu' className='menu'><span className='center'>Menu</span></a>
+    </>
+  )
+}
+
+export default index
--- src/objects/Sobre/index.js
+++ src/objects/Sobre/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import "./styles.css"
 
-
 const Sobre = () => {
   return (
     <a heref="#to-do " className= "sobre">Sobre</a>
